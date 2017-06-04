435eb99e18f2a442c07fc0740eac4fb0a11f3048
vinhch <vinhomn@gmail.com>
2017-06-04 19:03:46 +0700

    1

--- src/ReverseByteOrder/Program.cs
+++ src/ReverseByteOrder/Program.cs
@@ -19,7 +19,7 @@ namespace ReverseByteOrder
             var str =
                 "嬀儀䌀崀 䴀漀椀 儀甀礀 欀栀愀挀栀 最漀椀 ㄀　㘀㠀 ⠀㈀⸀㔀　　搀⼀瀀⤀ 搀攀 琀愀渀最 渀栀甀渀最 䴀伀一 儀唀䄀 礀 渀最栀椀愀 栀愀渀 渀最愀礀 嘀䄀䰀䔀一吀䤀一䔀 搀愀渀栀 挀栀漀 洀漀琀 渀甀愀 礀攀甀 琀栀甀漀渀最 挀甀愀 洀椀渀栀⸀ 吀甀 挀栀漀椀 儀䌀Ⰰ 漀愀渀 吀䌀㌀ 最甀椀 ㄀㤀㤀 ";
 
-            Func<string, string> reverseByteCode = (input) =>
+            Func<string, string> reverseByteOrder = (input) =>
             {
                 var result = new List<byte>();
                 foreach (var c in input)
@@ -31,7 +31,7 @@ namespace ReverseByteOrder
                 return Encoding.Unicode.GetString(result.ToArray());
             };
 
-            var strAfterReverseByte = reverseByteCode(str);
+            var strAfterReverseByte = reverseByteOrder(str);
             Console.WriteLine(strAfterReverseByte);
         }
     }
